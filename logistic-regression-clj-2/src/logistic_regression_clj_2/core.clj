b1865ec70ff182957259c9f2ffc3dafddcb2347b
Bill Piel <bpiel@rjmetrics.com>
2017-06-02 12:41:02 -0400

    logistic-regression-clj-2 multinomial works

--- logistic-regression-clj-2/src/logistic_regression_clj_2/core.clj
+++ logistic-regression-clj-2/src/logistic_regression_clj_2/core.clj
@@ -4,8 +4,50 @@
             [mikera.image.colours :as img-clr])
   (:gen-class))
 
+
+#_ (
+
+    (count
+     (img/get-pixels
+      (img/load-image-resource "notMNIST_small/A/SVRDR2FyYW1vbmRTdGQtQmQub3Rm.png")))
+
+    (apply max
+           (img/get-pixels
+            (img/load-image-resource "notMNIST_small/A/SSBqdXN0IGNhbGwudHRm.png")))
+
+    (println
+     (seq
+      (img/get-pixels
+       (img/load-image-resource "notMNIST_small/A/SSBqdXN0IGNhbGwudHRm.png"))))
+
+    (img/show
+     (img/load-image-resource "notMNIST_small/A/SSBqdXN0IGNhbGwudHRm.png"))
+
+
+    ;; create a new image
+    (def bi (img/new-image 32 32))
+
+    ;; gets the pixels of the image, as an int array
+    (def pixels (img/get-pixels bi))
+
+    ;; fill some random pixels with colours
+    (dotimes [i 1024]
+      (aset pixels i (img-clr/rand-colour)))
+
+    ;; update the image with the newly changed pixel values
+    (img/set-pixels bi pixels)
+
+    ;; view our new work of art
+    ;; the zoom function will automatically interpolate the pixel values
+    (img/show bi :zoom 10.0 :title "Isn't it beautiful?")
+
+    (take 100 (img/get-pixels bi))
+    (apply max (img/get-pixels bi))
+
+    (comment))
+
 (def letters ["A" "B" "C" "D" "E" "F" "G" "H" "I" "J"])
-(def letters-count 3 #_(count letters))
+(def letters-count (count letters))
 
 (file-seq (io/resource "notMNIST_small"))
 
@@ -48,20 +90,27 @@
 ;; Find the min and max values for each column
 (defn dataset-minmax
   [dataset]
-  (mapv (fn [[v]]
+  (mapv (fn [v]
           [(apply min v)
            (apply max v)])
-        (apply mapv vector dataset)))
+        (apply mapv vector (map first dataset))))
 
 ;; args must be vector of vectors -- no lists!!!
 (defn normalize-dataset
   [dataset minmax]
-  (for [row dataset]
-    (for [i (range (count row))]
-      (let [[mm0 mm1] (nth minmax i)]
-        (/ (- (nth row i)
-              mm0)
-           (- mm1 mm0))))))
+  (for [[row y] dataset]
+    (into
+     (vec (for [i (range (count row))]
+            (let [[mm0 mm1] (nth minmax i)
+                  xx (- mm1 mm0)]
+              (if (zero? xx)
+                0
+                (/ (- (nth row i)
+                      mm0)
+                   (- mm1 mm0))))))
+     [y])))
+
+(normalize-dataset $s/dataset1 $s/minmax)
 
 (defn cross-validation-split
   [dataset n-folds]
@@ -195,12 +244,12 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [ds (load-random-pngs 100)
+  (let [ds (load-random-pngs 200)
         mm (dataset-minmax ds)
         ds' (normalize-dataset ds mm)
         n-folds 5
         l-rate 0.1
-        n-epoch 1
+        n-epoch 4
         scores (evaluate-algorithm ds'
                                    logistic-regression
                                    n-folds
@@ -208,53 +257,14 @@
                                    n-epoch)]
     scores))
 
-
 #_ (-main)
 
-#_ (nth letters (predict2 (first (load-random-pngs* "C" 1))
+#_ (nth letters (predict2 (first (load-random-pngs* "I" 1))
                   @last-coef))
 
 
 
 
 
-#_ (
 
-    (count
-     (img/get-pixels
-      (img/load-image-resource "notMNIST_small/A/SVRDR2FyYW1vbmRTdGQtQmQub3Rm.png")))
-
-    (apply max
-           (img/get-pixels
-            (img/load-image-resource "notMNIST_small/A/SSBqdXN0IGNhbGwudHRm.png")))
-
-    (println
-     (seq
-      (img/get-pixels
-       (img/load-image-resource "notMNIST_small/A/SSBqdXN0IGNhbGwudHRm.png"))))
-
-    (img/show
-     (img/load-image-resource "notMNIST_small/A/SSBqdXN0IGNhbGwudHRm.png"))
-
-
-    ;; create a new image
-    (def bi (img/new-image 32 32))
-
-    ;; gets the pixels of the image, as an int array
-    (def pixels (img/get-pixels bi))
-
-    ;; fill some random pixels with colours
-    (dotimes [i 1024]
-      (aset pixels i (img-clr/rand-colour)))
-
-    ;; update the image with the newly changed pixel values
-    (img/set-pixels bi pixels)
-
-    ;; view our new work of art
-    ;; the zoom function will automatically interpolate the pixel values
-    (img/show bi :zoom 10.0 :title "Isn't it beautiful?")
-
-    (take 100 (img/get-pixels bi))
-    (apply max (img/get-pixels bi))
-
-    (comment))
+#_ (-main)
