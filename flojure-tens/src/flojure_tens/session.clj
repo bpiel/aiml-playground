7cd09d2dab2eb1be42277242538d2552ac4c35b1
Bill Piel <bpiel@rjmetrics.com>
2017-06-08 15:12:31 -0400

    ft -- SIGSEGV

--- flojure-tens/src/flojure_tens/session.clj
+++ flojure-tens/src/flojure_tens/session.clj
@@ -31,19 +31,30 @@
 ;; TODO use Graph doSync
 (defn- run* [^Session s ^RunRequest req]
   (let [g (:graph s)
-        outputs (long-array (-> req :fetch count))
+        outputs (long-array [(:handle (tsr/create-from-value 0))])
+        _ (clojure.pprint/pprint [(:handle s) 
+                                  (:options req)
+                                  (long-array []) ;; inputTensorHandles
+                                  (long-array []) ;; inputOpHandles
+                                  (int-array [])  ;; inputOpIndices
+                                  (long-array (->> req :fetch (mapv ops/compute-hash))) ;; outputOpHandles
+                                  (int-array [0] #_(map :idx (:fetch req))) ;; outputOpIndices
+                                  (long-array (map :handle (:targets req))) ;; targetOpHandles
+                                  (:return-meta req)
+                                  outputs])
         maybe-meta (tfnative.Session/run
                      (:handle s) 
                      (:options req)
                      (long-array []) ;; inputTensorHandles
                      (long-array []) ;; inputOpHandles
-                     (int-array []) ;; inputOpIndices
+                     (int-array [])  ;; inputOpIndices
                      (long-array (->> req :fetch (mapv ops/compute-hash))) ;; outputOpHandles
                      (int-array [0] #_(map :idx (:fetch req))) ;; outputOpIndices
                      (long-array (map :handle (:targets req))) ;; targetOpHandles
                      (:return-meta req)
-                     outputs)] 
-    {:outputs outputs
+                     outputs)]
+    #_    (clojure.pprint/pprint maybe-meta)
+    {:output-handles outputs
      :meta-data maybe-meta}))
 
 (defn run
